889bd9169f13172cfe768768f8f7608b04075494
William <xwilen22@gmail.com>
2021-01-01 16:30:43 +0100

    Added workbox framework

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,1 +1,7 @@
-console.log("HI :)))))")
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
+)
+  
